c3830dbdf34ae9db99ecfead5e2dc6a767b37e29
moment-13 <n.wisteria.14@gmail.com>
2023-05-09 17:15:02 +0900

    20230509

--- resources/js/Pages/Compornents/TodoForm.jsx
+++ resources/js/Pages/Compornents/TodoForm.jsx
@@ -32,10 +32,10 @@ export const TodoForm = (props) => {
         <>
           <Box sx={{ flexGrow: 1 }}>
            <Grid container alignItems='center' justifyContent='center' direction="column" spacing={2}>
-            <Grid xs={6} md={6}>
+            <Grid xs={4} md={4}>
               <TextField id="outlined-basic" label="タスクのタイトル" variant="outlined" onChange={e => setData('title', e.target.value)} fullWidth />
             </Grid>
-            <Grid xs={6} md={6}>
+            <Grid xs={4} md={4}>
               <TextField id="outlined-basic" label="タスクの詳細" variant="outlined"  onChange={e => setData('detail', e.target.value)} multiline fullWidth  minRows={3} />
             </Grid>
             <Grid xs={6} md={6} >
@@ -85,8 +85,8 @@ export const TodoForm = (props) => {
             </Grid>
             <Grid xs={6} md={6} >
             <div className='flex justify-center items-center'>
-            <label htmlFor="image_path" className='sr-only'>画像</label>
-            <input type="file" className="relative m-0 block min-w-0 flex-auto cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-blue-400 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" onChange={e => setData('image_path', e.target.files[0])} />
+            <label htmlFor="image_path" className='block text-sm text-gray-500 dark:text-gray-300'>画像</label>
+            <input type="file" className="relative m-0 block min-w-0 w-1/2 cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-blue-400 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" onChange={e => setData('image_path', e.target.files[0])} />
             {errors.image_path && <div>{errors.image_path}</div>}
             </div>
             </Grid>
